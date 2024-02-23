787985b120ce04a32db6a43a40bdace24f8babec
Chintan Kalasava <kalasavc@shernet.sheridancollege.ca>
2024-02-23 16:59:05 -0500

    Second step initialize is done

---
+++ MidTermPartC/src/main/java/com/mycompany/midtermpartc/MidTermPartC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.midtermpartc;
+
+/**
+ *
+ * @author knigh
+ */
+public class MidTermPartC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
