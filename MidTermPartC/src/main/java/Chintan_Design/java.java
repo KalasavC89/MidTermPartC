5d3695fafd12d10139d299b0233ae28536fe2bcd
Chintan Kalasava <kalasavc@shernet.sheridancollege.ca>
2024-02-23 17:41:31 -0500

    Third step initialize is done

---
+++ MidTermPartC/src/main/java/Chintan_Design/java.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chintan_Design;
+
+/**
+ *This is a comment
+ * My favorite sub is SOcial science
+ * @author knigh
+ */
+public class java {
+    
+}
